0169071a7dc77005e69c6f97a1feb48c30e3970a
mtaranetz <mtaranetz@yandex.ru>
2024-03-04 22:23:39 +0400

    Google Registration & account

--- app/src/main/java/com/example/vitamin/MainActivity.kt
+++ app/src/main/java/com/example/vitamin/MainActivity.kt
@@ -8,19 +8,30 @@ import android.view.animation.AnimationUtils
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var helloTextView: TextView
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val pearsImageView: ImageView = findViewById(R.id.main_pears)
         val pearsTurnAnimation = AnimationUtils.loadAnimation(this, R.anim.pears_animation)
         pearsImageView.startAnimation(pearsTurnAnimation)
+        auth = Firebase.auth
+
 
         helloTextView = findViewById(R.id.hellou)
+        helloTextView.setText(buildString {
+        append("Добрый день, ")
+    } + auth.currentUser?.displayName +"!")
         val transparencyAnimation = AnimationUtils.loadAnimation(this, R.anim.transparency)
         transparencyAnimation.setStartOffset(1500);
         helloTextView.startAnimation(transparencyAnimation)
---
+++ app/src/main/java/com/example/vitamin/RegistrationActivity.kt
@@ -0,0 +1,82 @@
+package com.example.vitamin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.vitamin.databinding.ActivityRegistrationBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class RegistrationActivity : AppCompatActivity() {
+    lateinit var launcher: ActivityResultLauncher<Intent>
+    lateinit var auth: FirebaseAuth
+    lateinit var binding: ActivityRegistrationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        checkAccount()
+
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+        try {
+            val account = task.getResult(ApiException::class.java)
+            if (account != null){
+                firebaseAuthWithGoogle(account.idToken!!)
+            }
+        } catch (e: ApiException) {Log.d("MyLog", "API")}}
+            binding.bSign.setOnClickListener(){
+                signWithGoogle()
+            }
+        }
+
+
+    private fun getClient(): GoogleSignInClient
+    {
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        return GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun signWithGoogle(){
+        val signInClient = getClient()
+        launcher.launch(signInClient.signInIntent)
+    }
+
+    private fun firebaseAuthWithGoogle(Token: String){
+        val credential = GoogleAuthProvider.getCredential(Token, null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if (it.isSuccessful){
+                Log.d("MyLog", "Done")
+                checkAccount()
+            }
+            else{ Log.d("MyLog", "Error")}
+        }
+
+    }
+
+    private fun checkAccount()
+    {
+        if (auth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
